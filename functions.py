5d8934ba1d86139c45f0078570ca72014c427344
dancingalfred <robban021@hotmail.com>
2022-10-07 10:52:00 +0200

    added removeDocumentById

--- functions.py
+++ functions.py
@@ -16,3 +16,13 @@ def updateDocumentById(databaseToUpdate:str, collectionToUpdate:str ,idToUpdate:
 
 # updateDocumentById("testData", "customers", "633fdc5d9e39c2cab67b7c9c", "name", "Robert")
 # updateDocumentById("testData", "customers", "633fdc5d9e39c2cab67b7c9c", "address", "Sigurdsgatan 20")
+
+#en funktion där man med hjälp av _id hittar och kör delete på ett dokument i en specifik collection
+def removeDocumentById(databaseToUpdate:str, collectionToUpdate:str ,idToUpdate:str, iAmSure:bool):
+    database = myclient[databaseToUpdate]
+    collection = database[collectionToUpdate]
+    query = {'_id': ObjectId(idToUpdate)}
+    if iAmSure == True:
+        collection.delete_one(query)
+
+#removeDocumentById("testData", "customers", "633fe16101d1eec1c4dc560f", True)
